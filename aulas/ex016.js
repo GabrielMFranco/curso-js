81d04921005112d9cf35071b0e94286ece680f9f
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-05 20:31:36 -0300

    ex016

---
+++ aulas/ex016.js
@@ -0,0 +1,11 @@
+var c = 1
+var d = 1
+while (c <= 1 ){
+    console.log(`Olá mundo! ${c}`)
+    c++
+}
+do{
+    console.log(`Passo ${d}`)
+    c++
+}while(c <=10)
+    
---
+++ aulas/node_2f912e0e73136.tmp.js
@@ -0,0 +1,9 @@
+var c = 1
+var d = 1
+while (c <= 1 ){
+    console.log(`Olá mundo! ${c}`)
+    c++
+}
+do{
+    console.log(`Passo ${d}`)
+}while(c <=10)
