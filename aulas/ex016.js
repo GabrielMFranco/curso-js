9bf2ac46eb79da0548c2c198a68dd3bebc31dda2
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-05 21:19:10 -0300

    ex016 e 017

--- aulas/ex016.js
+++ aulas/ex016.js
@@ -1,11 +1,11 @@
 var c = 1
 var d = 1
-while (c <= 1 ){
+while (c < 1 ){
     console.log(`Olá mundo! ${c}`)
     c++
 }
 do{
     console.log(`Passo ${d}`)
     d++
-}while(d <=10)
+}while(d <1)
     
