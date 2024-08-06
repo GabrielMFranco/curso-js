fb22e6f39f1a7903290646110af0e44f6738ab4a
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-05 22:26:37 -0300

    .

--- aulas/ex016.js
+++ aulas/ex016.js
@@ -1,11 +1,20 @@
 var c = 1
 var d = 1
-while (c < 1 ){
+//loop while
+while (c <= 5 ){
     console.log(`Olá mundo! ${c}`)
     c++
 }
+
+// loop do while
 do{
     console.log(`Passo ${d}`)
     d++
-}while(d <1)
-    
+}while(d < 5)
+
+// loop for
+for(var n = 1; n <=5; n++){
+    console.log(n)
+}
+
+
---
+++ aulas/ex017/ex017p1/script.js
@@ -0,0 +1,11 @@
+function contagem(){
+    var i= document.getElementById('inicio')
+    var f= document.getElementById('fim')
+    var p= document.getElementById('passo')
+
+    window.alert(i)
+
+   /* if(i.value.length<0 || f.value.length==0 || p.value.length==0){
+        window.alert('ERRO, tente novamente')
+    }*/
+}
