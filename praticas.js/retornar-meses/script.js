ce9dc855c39804c4a44844928eb35abf4e831a06
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-08 14:31:53 -0300

    pratica.js

---
+++ praticas.js/retornar-meses/script.js
@@ -0,0 +1,10 @@
+var a =  10
+var b =  10
+
+if(a < b){
+    console.log('O primeiro número é menor')
+}else if(a > b){
+    console.log('O primeiro número é maior')
+}else{
+    console.log('os números são iguais')
+}
