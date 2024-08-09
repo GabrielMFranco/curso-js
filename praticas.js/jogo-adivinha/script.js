0d2069625952c2f19805a0b7c85c8b238c9814e4
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-09 16:04:23 -0300

    jogo

---
+++ praticas.js/jogo-adivinha/script.js
@@ -0,0 +1,23 @@
+var comp = ''
+var player = ''
+
+function gerar(){
+    var min = 1
+    var max = 100
+    var dif = max - min
+    var randomic = Math.random()
+    comp= min + Math.trunc(dif * randomic)
+}
+function jogo(){
+    player = Number(window.prompt('Qual é seu palpite?'))
+    if(player < comp){
+        res.innerHTML += '<p> Chute um número maior</p>'  
+    }else if(player > comp){
+        res.innerHTML += "<p>Chute um número menor</p>"
+    }else if(player == comp){      
+        res.innerHTML += '<p>Você acertou</p>'
+         document.getElementById('botao').style.visibility = 'hidden'
+        
+    }   
+}
+
