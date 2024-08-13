f2c97f341447fccd3ab260ffece9d2c08ce1098c
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-13 16:39:06 -0300

    jogo adivinha

--- praticas.js/jogo-adivinha/script.js
+++ praticas.js/jogo-adivinha/script.js
@@ -2,18 +2,18 @@ var comp = ''
 var player = ''
 
 function gerar(){
-    var alea = Math.floor(Math.random()*100)+1;
-    comp = alea
+    var alea = Math.floor(Math.random()*100);
+    comp = alea + 1
 }
 function jogo(){
     gerar()
     player = Number(window.prompt('Qual é seu palpite?'))
     if(player < comp){
-        res.innerHTML += `<p>Chute um número maior</p>`
+        res.innerHTML += `<p>${player} Chute um número maior</p>`
     }else if(player > comp){
-        res.innerHTML += `<p>Chute um número menor</p>`
-    }else if(player == comp){      
-        res.innerHTML += `<p>Você acertou</p>`
+        res.innerHTML += `<p>${player} Chute um número menor</p>`
+    }else{      
+        res.innerHTML += `<p>${player} Você acertou</p>`
          document.getElementById('botao').style.visibility = 'hidden'
         
     }   
