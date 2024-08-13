191a7d2b6d0cff5328dcc53fb58e060bdcefb4ed
GabrielMFranco <gabri.m.franco@gmail.com>
2024-08-13 16:19:45 -0300

    corrigi erro jogo

--- praticas.js/jogo-adivinha/script.js
+++ praticas.js/jogo-adivinha/script.js
@@ -2,20 +2,18 @@ var comp = ''
 var player = ''
 
 function gerar(){
-    var min = 1
-    var max = 100
-    var dif = max - min
-    var randomic = Math.random()
-    var comp= min + Math.trunc(dif * randomic)
+    var alea = Math.floor(Math.random()*100)+1;
+    comp = alea
 }
 function jogo(){
+    gerar()
     player = Number(window.prompt('Qual é seu palpite?'))
     if(player < comp){
-        res.innerHTML += '<p> Chute um número maior</p>'  
+        res.innerHTML += `<p>Chute um número maior</p>`
     }else if(player > comp){
-        res.innerHTML += "<p>Chute um número menor</p>"
+        res.innerHTML += `<p>Chute um número menor</p>`
     }else if(player == comp){      
-        res.innerHTML += '<p>Você acertou</p>'
+        res.innerHTML += `<p>Você acertou</p>`
          document.getElementById('botao').style.visibility = 'hidden'
         
     }   
